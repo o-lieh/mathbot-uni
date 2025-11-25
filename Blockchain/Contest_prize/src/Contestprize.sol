f2ca9055004bd5239960e14e34a7f19f7f083510
ATH <aria123sparco@gmail.com>
2025-11-25 19:34:59 +0330

    create contract and add a struct for each comp

---
+++ Blockchain/Contest_prize/src/Contestprize.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract ContestPrize is Ownable {
+    constructor() Ownable(msg.sender) {}
+
+    struct comp {
+        uint256 Total_amount;
+        uint256 Price;
+        bool status;
+        bool exist;
+    }
+}
